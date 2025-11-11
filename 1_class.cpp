bb8959d5e4ce6e1bae339125228bf0e30d447a84
Balu Sharma Gotety <61464599+gotetybalu@users.noreply.github.com>
2025-11-11 11:13:56 +0530

    Add student class with display method

--- 1_class.cpp
+++ 1_class.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+class student
+{
+    public:
+        string name;
+        int roll;
+        void display()
+        {
+            cout<<"student name is "<<name<<endl;
+            cout<<"Roll number"<<roll<<endl;
+        }
+};
+
+int main()
+{
+    student s1;
+    s1.name="Ram";
+    s1.roll=123;
+    s1.display();
+}
